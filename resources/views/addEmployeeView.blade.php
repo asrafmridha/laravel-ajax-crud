7f7956fe5ca607154f4ab8bf1507d640241e3514
asraf-mridha <asrafmridha@gmail.com>
2022-08-15 23:19:08 +0600

    Create form for employee

---
+++ resources/views/addEmployeeView.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+   
+
+<div class="row mt-5">
+
+   
+<div class="col-md-3 offset-md-1 ">
+
+
+<div class="form-group">
+<label for="exampleInputEmail1">First Name</label>
+<input type="text" class="fname form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your First Name"  >
+
+</div>
+<div class="form-group">
+  <label for="exampleInputEmail1">Last Name</label>
+  <input type="text" class="lname form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Your Last Name"  >
+  
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Address</label>
+    <textarea name="" id="" cols="30" rows="1" class="address form-control" placeholder="Enter Your Address">
+
+    </textarea>
+    
+    </div>
+<div class="form-group">
+<label for="exampleInputPassword1">Phone</label>
+<input type="number" class="phone form-control" id="exampleInputPassword1" placeholder="Enter Phone Number" >
+</div>
+
+
+
+<div class="form-group">
+<label for="exampleInputPassword1">Phone</label>
+<input type="email" class="email form-control" id="exampleInputPassword1" placeholder="Enter Your email" >
+</div>
+
+
+<div class="form-group">
+<label for="exampleInputPassword1">Status</label>
+<select class="form-control"  id="">
+  <option value="#">---Select--</option>
+<option value="1">Active</option>
+<option value="2">Inactive</option>
+
+</select>
+</div> 
+
+<button name="save" type="submit" class="btn btn-primary mt-3">Submit</button>
+
+
+</div>
+</div>
+
+</form>
+
+
+
+
+
+
+</body>
+</html>
